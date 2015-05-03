1124e2f13cf3a4dc86e99b65519a25f577ab98eb
Alexander Troshin <alexander@troshin.eu>
2015-05-03 19:52:32 +0200

    Reworked router, header transitions, fixes

--- common.js
+++ common.js
@@ -44,3 +44,22 @@ Schemas.Pastes = new SimpleSchema({
 
 // Initiate schemas
 Pastes.attachSchema(Schemas.Pastes);
+
+// Configuration
+
+AccountsTemplates.configure({
+    // Behaviour
+    enablePasswordChange: true,
+
+    // Appearance
+    showForgotPasswordLink: true,
+    showReCaptcha: true,
+
+    // Privacy Policy and Terms of Use
+    privacyUrl: 'privacy',
+    termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/',
+    redirectTimeout: 4000
+});
